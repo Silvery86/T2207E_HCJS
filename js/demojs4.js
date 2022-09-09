1fab2e8668bacb1f1d1085023220e8f02ded1a29
Silvery86 <giang8692@gmail.com>
2022-09-10 00:25:14 +0700

    up

---
+++ js/demojs4.js
@@ -0,0 +1,22 @@
+function update(city) {
+    var bg = document.getElementById("bg-fade");
+    bg.style.display = "block";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+        console.log(this.responseText);
+        // convert to object of js
+        var data = JSON.parse(this.responseText);
+        // console.log(data.main.temp);
+        var t = data.main.temp;// 26
+        var x1 = document.getElementById("temp");
+        x1.innerText = t;
+        var ct = data.name + ", " + data.sys.country;
+        document.getElementById("city").innerText = ct;
+        var ps = data.main.pressure;
+        document.getElementById("press").innerText = ps;
+        bg.style.display = "";
+    };
+    var link = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=09a71427c59d38d6a34f89b47d75975c&units=metric";
+    xhttp.open("get", link, true);
+    xhttp.send();
+}
